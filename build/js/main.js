d1105160507835447d3d6dcca9a666d12f99df07
KrasavinSerg <krasavin.serg@gmail.com>
2018-12-15 02:20:05 +0300

    mask added

--- build/js/main.js
+++ build/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('.regulation__title').click(function() {
     $('.regulation__text').toggle('regulation__open');
   });
+
+  $("#phone, #phone-modal").mask("+7 (999) 999-99-99");
+  
 });
